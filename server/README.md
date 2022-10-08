3ffdb0598763510476e4adf01ececfd8c95d288e
roshiii24 <rrsaggarwal@gmail.com>
2022-10-08 11:22:44 +0530

    completed setup of server and added babel to it

---
+++ server/README.md
@@ -0,0 +1,9 @@
+#API PLANNING
+
+- Food (food items and their details)
+- Restaurant (restaurant and their details)
+- Menu (menu and its details)
+- Order (Order and its details)
+- Reviews (storing all the list of reviews)
+- Image (storing all the images related to the project)
+- User (user related details, username, email, password)
---
+++ server/database/allModels.js
@@ -0,0 +1,17 @@
+import { FoodModel } from "./food";
+import { RestaurantModel } from "./restaurant";
+import { ImageModel } from "./image";
+import { MenuModel } from "./menu";
+import { OrderModel } from "./order";
+import { ReviewModel } from "./review";
+import { UserModel } from "./user";
+
+export {
+    FoodModel,
+    RestaurantModel,
+    ImageModel,
+    MenuModel,
+    OrderModel,
+    ReviewModel,
+    UserModel,
+};
